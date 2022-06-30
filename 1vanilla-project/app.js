0d50c45c2122a5c898b055b3ea96d1d2a82aab65
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-06-30 11:35:29 +0300

    completed site

--- 1vanilla-project/app.js
+++ 1vanilla-project/app.js
@@ -5,8 +5,9 @@ arrows.forEach((arrow, i) => {
     const itemNumber = movieLists[i].querySelectorAll("img").length;
     let clickCounter = 0;
     arrow.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth / 270);
         clickCounter++;
-        if (itemNumber - (4 + clickCounter) >= 0) {
+        if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
             movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value - 300}px)`;
         } else {
             movieLists[i].style.transform = "translateX(0)";

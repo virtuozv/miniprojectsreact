13e0f670a1cb00e327607c4ac3610ff038f69a1f
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-07-04 15:24:47 +0300

    okay dev liked hook

--- 3-ui-design-sn/src/components/post/Post.jsx
+++ 3-ui-design-sn/src/components/post/Post.jsx
@@ -1,8 +1,17 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 import { Users } from "../../dummyData";
+import { useState } from "react";
 
 const Post = ({ item }) => {
+  const [like, setLike] = useState(item.like);
+  const [isLiked, setIsLiked] = useState(false);
+
+  const likeHandler = () => {
+    setLike(isLiked ? like - 1 : like + 1);
+    setIsLiked(!isLiked);
+  };
+
   return (
     <>
       <div className="postContainer">
@@ -28,9 +37,19 @@ const Post = ({ item }) => {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like.png" alt="" />
-            <img className="likeIcon" src="assets/heart.png" alt="" />
-            <span className="postLikeCounter">{item.like} people liked it</span>
+            <img
+              className="likeIcon"
+              src="assets/like.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src="assets/heart.png"
+              onClick={likeHandler}
+              alt=""
+            />
+            <span className="postLikeCounter">{like} people liked it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">{item.comment} comments</span>

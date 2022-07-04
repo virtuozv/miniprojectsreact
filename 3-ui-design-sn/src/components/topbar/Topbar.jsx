691a67be0765ca85704ecf1025e304941ec3791d
Pavel Strutinsky <pvorobiev@mmtr.ru>
2022-07-04 11:30:38 +0300

    dev topbar

---
+++ 3-ui-design-sn/src/components/topbar/Topbar.jsx
@@ -0,0 +1,43 @@
+import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+
+const Topbar = () => {
+  return (
+    <div>
+      <div className="topbar-container">
+        <div className="topBarLeft">
+          <div className="logo">Social Network</div>
+        </div>
+        <div className="topBarCenter">
+          <div className="searchbar">
+            <Search className="searchIcon" />
+            <input
+              placeholder="Search for friend, post or video"
+              className="searchInput"
+            />
+          </div>
+        </div>
+        <div className="topBarRight">
+          <div className="topbarLinks">
+            <div className="topbarLink">Homepage</div>
+            <div className="topbarLink">Timeline</div>
+          </div>
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <Person /> <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Chat /> <span className="topbarIconBadge">2</span>
+            </div>
+            <div className="topbarIconItem">
+              <Notifications /> <span className="topbarIconBadge">3</span>
+            </div>
+          </div>
+          <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
---
+++ 3-ui-design-sn/src/pages/home/Home.js
@@ -0,0 +1,8 @@
+import Topbar from '../../components/topbar/Topbar';
+const Home = () => {
+    return <div>
+        <Topbar />
+    </div>
+}
+
+export default Home
